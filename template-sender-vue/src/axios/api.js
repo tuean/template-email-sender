0bc357c02a699e993a802a2abb3e6c84ecb87156
tuean_z@163.com <tuean330330>
2019-09-24 22:52:19 +0800

    配置axios

---
+++ template-sender-vue/src/axios/api.js
@@ -0,0 +1,16 @@
+import { get, post } from "./axios";
+
+//获取程序配置
+export function getConfig() {
+  return get("static/config.json", null, { baseURL: "./" });
+}
+
+// 查找货物已占用位置
+export function queryGoodsLabel(params) {
+  return get("/goods/queryGoodsLabel", params);
+}
+
+// 更换货物已占用位置
+export function switchLabel(params) {
+  return post("/goods/switchLabel", params);
+}
