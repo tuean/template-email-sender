b5adcb96a36ea424d5e8a692c4fb408b46b9f322
tuean_z@163.com <tuean330330>
2019-09-26 23:29:35 +0800

    work at home

--- template-sender-vue/src/main.js
+++ template-sender-vue/src/main.js
@@ -5,8 +5,13 @@ import App from './App'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.use(Element)
+Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
